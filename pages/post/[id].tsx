184f664d0b6a9b0fe20fc85c8c0cc785eda7594c
eee <hlxsmail@gmail.com>
2021-11-15 14:18:34 +0800

    add new page

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -2,7 +2,7 @@ import {blogApi} from "../../util/req";
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 import 'github-markdown-css/github-markdown.css'
-import {Heading} from "@chakra-ui/react";
+import {Container, Heading} from "@chakra-ui/react";
 import React from "react";
 import {BlogData} from "dd_server_api_web/apis/model/result/BlogPushNewResultData";
 import {GetServerSideProps, GetServerSidePropsContext, PreviewData} from "next";
@@ -10,13 +10,15 @@ import {ParsedUrlQuery} from "querystring";
 
 
 const Blog: React.FC<{ blog: BlogData }> = ({blog}) => {
-    return <div>
-        <Heading>{blog.title}</Heading>
-        <div className={'markdown-body'}>
-            {/* eslint-disable-next-line react/no-children-prop */}
-            <ReactMarkdown children={blog.content} remarkPlugins={[remarkGfm]}/>
-        </div>
-    </div>
+    return <Container maxW={'container.md'}>
+        <article>
+            <Heading>{blog.title}</Heading>
+            <div className={'markdown-body'}>
+                {/* eslint-disable-next-line react/no-children-prop */}
+                <ReactMarkdown children={blog.content} remarkPlugins={[remarkGfm]}/>
+            </div>
+        </article>
+    </Container>
 }
 
 class CustomParsedUrlQuery implements ParsedUrlQuery {
